d6d3a10de2d67520acfcddf799a51d75efc02b5b
Silence <294005139@qq.com>
2023-06-18 12:27:55 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,19 +7,14 @@ let package = Package(
     name: "HXPhotoPicker",
     platforms: [.iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "HXPhotoPicker",
             targets: ["HXPhotoPicker"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HXPhotoPicker",
             dependencies: ["Kingfisher"],
