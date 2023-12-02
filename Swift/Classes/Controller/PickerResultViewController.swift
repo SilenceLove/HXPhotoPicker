9a74c78413bb4efbfc392830d7e843120440485f
Silence <294005139@qq.com>
2023-12-02 10:44:38 +0800

    Update PickerResultViewController.swift

--- Swift/Classes/Controller/PickerResultViewController.swift
+++ Swift/Classes/Controller/PickerResultViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 import Photos
 import HXPhotoPicker
 import Kingfisher
-import AVKit
 
 class PickerResultViewController: UIViewController,
                                   UICollectionViewDataSource,
@@ -461,7 +460,6 @@ class PickerResultViewController: UIViewController,
 //        )
         if #available(iOS 13.0, *) {
             Task {
-                var videoURL: URL?
                 for (index, selectedAsset) in selectedAssets.enumerated() {
                     do {
                         print("第" + String(index + 1) + "个")
@@ -478,9 +476,6 @@ class PickerResultViewController: UIViewController,
                                 "本地图片地址" : "本地视频地址",
                                 result.url
                             )
-                            if result.mediaType == .video {
-                                videoURL = result.url
-                            }
                         }
                     } catch {
                         print("地址获取失败: ", error)
@@ -488,11 +483,6 @@ class PickerResultViewController: UIViewController,
                 }
                 view.hx.hide(animated: false)
                 view.hx.showSuccess(text: "获取完成", delayHide: 1.5, animated: true)
-                if let videoURL {
-                    let vc = AVPlayerViewController()
-                    vc.player = .init(url: videoURL)
-                    self.present(vc, animated: true)
-                }
             }
         } else {
             selectedAssets.getURLs(
